d4ac4021f192f0646f7187bace3f13fd6eef559d
yiannis <yiannis.chang@gmail.com>
2022-03-18 14:33:46 +0800

    on client and server

--- src/browser/nlp.tsx
+++ src/browser/nlp.tsx
@@ -1,33 +1,58 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import * as $ from 'jquery';
 
-import Verbs from 'compromise/types';
-import Document from 'compromise/types';
-import DefaultDocument from 'compromise';
+// import Verbs from 'compromise/types';
+// import Document from 'compromise/types';
+// import DefaultDocument from 'compromise';
 
 const nlp = require('compromise');
+
 /**
  * Text process
+ * 
+ * A simple version could be used in browser,
+ * a advanced version should be in server.
+ * 
+ * let client script to check this.
+ * 
  */
-nlp
+
 const App = () => {
 
-    let doc = nlp($('body').text());
+    // do it in browser, or on the server
 
-    const onClick = () => {
+    const onClickServer = () => {
+
+        $.ajax({
+            url: '/res/nlp'
+        }).done((data) => {
+            console.log(data);
+        });
+
+    }
+
+    const onClickClient = () => {
+        let doc = nlp($('body').text());
         let result = doc.verbs().out('array');
-
         console.log(result);
     }
 
     return (
-        <Button
-            onClick={onClick}
-        >
-            NLP
-        </Button>
+        <Box>
+            <Button
+                onClick={onClickServer}
+            >
+                NLP-Server
+            </Button>
+            <Button
+                onClick={onClickClient}
+            >
+                NLP-Client
+            </Button>
+        </Box>
     );
 }
 
@@ -41,4 +66,4 @@ export class Nlp {
         ReactDOM.render(<App/>, document.querySelector(`#${this.containerId}`));
     }
 }
-//
+
--- src/node/server/server.ts
+++ src/node/server/server.ts
@@ -1,7 +1,10 @@
 
-
 import * as express from "express";
 import * as path from 'path';
+import * as fs from 'fs';
+import * as util from 'util';
+import * as cheerio from "cheerio";
+const nlp = require('compromise');
 
 export function startServer() {
 
@@ -13,12 +16,50 @@ export function startServer() {
 
     app.get('/res', (req, res) => {
         res.sendFile('index.html', {root: root});
+    });
+
+    /**
+     * nlp process
+     */
+    app.get('/res/nlp', async (req, res) => {
+        
+        cleanText(path.join(root, '/res/res/thinking/A Rulebook for Arguments.18.html'))
+            .then((result) => {
+                
+                res.send(getVerbs(result));
+            
+            }).catch(() => res.send('error'));
+
     })
 
     app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-    })
+        console.log(`Example app listening on port ${port}`)
+    });
 
 } 
 
 
+async function cleanText(path: string): Promise<string> {
+
+    const readFile = util.promisify(fs.readFile);
+
+    return new Promise((resolve, reject) => {
+        readFile(path).then((data) => {
+            const $ = cheerio.load(data.toString());
+            const text = $('body').text();
+
+            // do the clean
+
+
+            resolve(text);
+ 
+        }).catch((error) => {
+            reject();
+        })
+    });
+
+}
+
+function getVerbs(text: string) {
+    return nlp(text).verbs().out('array');
+}
