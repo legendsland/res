7c03ae4192ab55ed001b95015baf9aa82f459682
legendsland <legendsland@gmail.com>
2025-03-29 00:33:56 +0800

    update

--- src/node/cli/gen.ts
+++ src/node/cli/gen.ts
@@ -2,6 +2,7 @@ import { ParsedPath } from 'path';
 import * as path from 'path';
 import { fromIgnoreFile } from './ignorefile';
 import { NodeDb } from '../server/res';
+import { Note } from '../../common/db';
 
 const fs = require('fs');
 
@@ -13,17 +14,26 @@ export async function createIndex() {
         data: list,
     };
 
-    const db_: {db: NodeDb} = await require('./db');
+    const db_: { db: NodeDb } = await require('./db');
     const { db } = db_;
 
-    list.forEach((l: ParsedPath & {stars: number, note: number}) => {
-        const url = encodeURI(path.join('/res/', l.dir, l.base));
-        const notes = db.getAnn(url);
+    list.forEach((l: ParsedPath & { stars: number, note: number }) => {
+        let notes: Note[] = [];
+        if (l.base?.endsWith('.html')) {
+            const htmlUrl = encodeURI(path.join('/res/', l.dir, l.base));
+            notes = db.getAnn(htmlUrl);
+        }
+        if (l.base?.endsWith('.pdf')) {
+            const pdfUrl = encodeURI(path.join(`/res/pdf.html?pdf=${l.dir}`, l.base));
+            notes = db.getAnn(pdfUrl);
+        }
         let maxStars = 0;
         notes.forEach((n) => {
             let stars = 0;
             for (let i = 0; i < n.note.length; ++i) {
                 if (n.note[i] === '⭐') {
+                    // debug
+                    // console.log(l.base);
                     ++stars;
                 }
             }
