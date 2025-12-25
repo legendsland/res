fe6d142d77a509851366b22e0e3b1376ec6ea68a
legendsland <legendsland@gmail.com>
2025-12-25 20:20:49 +0800

    --- update all --

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,6 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import importPlugin from 'eslint-plugin-import';
-import airbnb from 'eslint-config-airbnb';
-import { fixupConfigRules } from '@eslint/compat';
 
 export default defineConfig(
     eslint.configs.recommended,
--- src/node/common/util.ts
+++ src/node/common/util.ts
@@ -1,5 +1,1 @@
-
-
 export const ROOT = '/home/zy/ws/res';
-
-
